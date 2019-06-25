b7147bd5cf8a726509c9afb7787450ed956304ff
rabinson.ghatani <rabinson.ghatani>
2019-06-25 13:31:01 +0545

    added application controller,migration,model

---
+++ database/migrations/2019_06_25_073940_create_applications_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function(Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->increments('application_id');
+            $table->integer('product')->unsigned()->nullable();
+            $table->integer('user')->unsigned()->nullable();
+            $table->dateTime('applied_date')->nullable();
+            $table->string('status', 45)->nullable();
+            $table->dateTime('approved_at')->nullable();
+            $table->integer('approved_by')->nullable();
+            $table->enum('applications_status', ['pending',  'approved',  'rejected'])->nullable();
+
+            $table->index('product','product_idx');
+            $table->index('user','user_idx');
+
+            $table->foreign('product')
+                ->references('product_id')->on('products');
+
+            $table->foreign('user')
+                ->references('user_id')->on('users');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
